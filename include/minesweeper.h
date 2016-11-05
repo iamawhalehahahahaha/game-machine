c1e00e750b85c4b043fec8695ad2f6ac3793f870
John West <iamawhalehahahahaha@gmail.com>
2016-11-04 20:45:16 -0500

    Well, I'm going to figure this out eventually.

--- include/minesweeper.h
+++ include/minesweeper.h
@@ -22,10 +22,11 @@ public:
     Msweep();
     Msweep(std::size_t rows, std::size_t columns, std::size_t mines);
     ~Msweep();
+
 private:
-    void insertMines(std::size_t start_row, std::size_t start_column);
-    void updateMine(std::size_t start_row, std::size_t start_column);
-    void floodfill(std::size_t start_row, std::size_t start_column);
+    void insertMines(std::size_t row, std::size_t column);
+    void updateMine(std::size_t row, std::size_t column);
+    void floodfill(std::size_t row, std::size_t column);
     bool sweep(std::size_t test_row, std::size_t test_column);
     bool flag(std::size_t flag_row, std::size_t flag_column);
     void end(bool win);
